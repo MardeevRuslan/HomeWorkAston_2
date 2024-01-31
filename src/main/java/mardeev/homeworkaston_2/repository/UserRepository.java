c1a1d88ae1729bbf30bf1f7fa4eaf52f07560fe1
mardeev.ry <mardeev.ry@ZSA>
2024-01-31 12:21:17 +0700

    Spring Boot

--- src/main/java/mardeev/homeworkaston_2/repository/UserRepository.java
+++ src/main/java/mardeev/homeworkaston_2/repository/UserRepository.java
@@ -5,7 +5,7 @@ import mardeev.homeworkaston_2.entity.User;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository {
+public interface UserRepository  {
 
     Optional<User> findByName(String name);
 
